3e3852f0881543dd10897938918bf295086a3b1c
nilsir <nilsir@qq.com>
2018-08-22 09:31:22 +0000

    Apply fixes from StyleCI

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the nilsir/weather.
+ *
+ * (c) nilsir <nilsir@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Nilsir\Weather;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
